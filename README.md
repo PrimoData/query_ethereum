4e8e7696741c9ce250a3efb6ca2129952ef09e6c
Primo Data <mershon.max16@gmail.com>
2023-06-19 15:10:13 +0000

    updates

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# blockchain_query_editor
+# Query Ethereum
+
+[![GitHub Codespaces Ready](https://img.shields.io/badge/GitHub%20Codespaces-Ready-green)](https://github.com/features/codespaces)
+[![Code style: black](https://img.shields.io/badge/Code%20style-black-000000.svg)](https://github.com/psf/black)
+
+Quickly explore Ethereum blockchain data w/ different providers. For extensive usage, register directly with providers.
+
+Supported Blockchain Data Providers:
+* [Chainbase](https://chainbase.online/)
+* [Flipside](https://flipsidecrypto.xyz/)
+* [Transpose](https://www.transpose.io/)
--- app.py
+++ app.py
@@ -2,12 +2,19 @@ import streamlit as st
 from streamlit_ace import st_ace
 import time
 import os
-
+import pandas as pd
+from shroomdk import ShroomDK
+from transpose import Transpose
+import requests
+import json
+import math
 
 # Configure Streamlit Page
-page_icon = "assets/img/database.svg"
-st.set_page_config(
-    page_title="Blockchain Query Editor", page_icon=page_icon, layout="wide"
+page_icon = "assets/img/eth.jpg"
+st.set_page_config(page_title="Query ETH", page_icon=page_icon, layout="wide")
+st.header("Query Ethereum")
+st.warning(
+    "Quickly explore Ethereum blockchain data w/ different providers. For extensive usage, register directly with providers."
 )
 
 # Read Custom CSS
@@ -16,49 +23,133 @@ with open("assets/css/style.css", "r") as f:
 custom_css = f"<style>{css_text}</style>"
 st.markdown(custom_css, unsafe_allow_html=True)
 
-
 # Get API Keys
-flipside_api_key = os.environ["FLIPSIDE_KEY"]
-transpose_api_key = os.environ["TRANSPOSE_KEY"]
-chainbase_api_key = os.environ["CHAINBASE_KEY"]
+flipside_key = os.environ["FLIPSIDE_KEY"]
+transpose_key = os.environ["TRANSPOSE_KEY"]
+chainbase_key = os.environ["CHAINBASE_KEY"]
+
+
+# Query Flipside using their Python SDK
+def query_flipside(q):
+    sdk = ShroomDK(flipside_key)
+    result_list = []
+    for i in range(1, 11):  # max is a million rows @ 100k per page
+        data = sdk.query(q, page_size=100000, page_number=i)
+        if data.run_stats.record_count == 0:
+            break
+        else:
+            result_list.append(data.records)
+    result_df = pd.DataFrame()
+    for idx, each_list in enumerate(result_list):
+        if idx == 0:
+            result_df = pd.json_normalize(each_list)
+        else:
+            try:
+                result_df = pd.concat([result_df, pd.json_normalize(each_list)])
+            except:
+                continue
+    result_df.drop(columns=["__row_index"], inplace=True)
+    return result_df
+
+
+# Query Transpose using their API
+def query_transpose(q):
+    url = "https://api.transpose.io/sql"
+    headers = {
+        "Content-Type": "application/json",
+        "X-API-KEY": transpose_key,
+    }
+    response = requests.post(
+        url,
+        headers=headers,
+        json={
+            "sql": q,
+        },
+    )
+    results_json = json.loads(response.text)["results"]
+    results_df = pd.DataFrame.from_dict(results_json)
+    return results_df
+
+
+# Query Chainbase using their REST API
+def query_chainbase(q):
+    url = "https://api.chainbase.online/v1/dw/query"
+    payload = {"query": q}
+    headers = {"x-api-key": chainbase_key}
+    response = requests.post(url, json=payload, headers=headers)
+    task_id = response.json()["data"]["task_id"]
+    row_ct = response.json()["data"]["rows"]
+    all_results_df = pd.DataFrame([])
+    row_ct = math.ceil(response.json()["data"]["rows"] / 1000)
+    if row_ct == 1:
+        data = response.json()["data"]["result"]
+        all_results_df = pd.json_normalize(data)
+    else:
+        time.sleep(2)
+        for i in range(0, row_ct, 1):
+            payload = {"task_id": task_id, "page": i + 1}
+            response = requests.post(url, json=payload, headers=headers)
+            data = response.json()["data"]["result"]
+            results_df = pd.json_normalize(data)
+            all_results_df = pd.concat([all_results_df, results_df])
+            time.sleep(2)
+    return all_results_df
+
+
+# Provider names mapped to their respective query functions
+def run_query(q, provider):
+    provider_query = {
+        "Flipside": query_flipside,
+        "Transpose": query_transpose,
+        "Chainbase": query_chainbase,
+    }
+    df = provider_query[provider](q)
+    return df
+
 
 # Fetch data
-schema_df = load_data()
-
-# Render the Query Editor
-table_values = sorted(schema_df["Tables"].unique())
+schema_df = pd.read_csv("assets/provider_schema_data.csv")
 
 # Sidebar
-st.sidebar.image("assets/img/cusp.png", width=150)
-st.sidebar.header("Query CUSP Data")
-for table_name in table_values:
+st.sidebar.image("assets/img/ethereum.jpg", width=300)
+provider = st.sidebar.selectbox("Providers", ["Flipside", "Transpose", "Chainbase"])
+st.sidebar.write("Tables")
+
+# Render the Query Editor
+provider_schema_df = schema_df[schema_df["datawarehouse"] == provider]
+provider_tables_df = (
+    provider_schema_df.drop(columns=["column_name"])
+    .drop_duplicates()
+    .sort_values(by=["table_name"])
+)
+
+for index, row in provider_tables_df.iterrows():
+    table_name = row["table_name"]
+    table_schema = row["table_schema"]
+    table_catalog = row["table_catalog"]
+    if str(table_catalog) != "nan":
+        table_catalog = f"{table_catalog}."
+    else:
+        table_catalog = ""
+
     with st.sidebar.expander(table_name):
-        st.code(f"`censusdata-team3si.elds_db.{table_name}`", language="sql")
-        columns_df = schema_df[schema_df["Tables"] == table_name][["Columns"]]
+        st.code(f"{table_catalog}{table_schema}.{table_name}", language="sql")
+        columns_df = provider_schema_df[provider_schema_df["table_name"] == table_name][
+            ["column_name"]
+        ]
         st.table(columns_df)
+
 
 # Query Editor
 ace_query = st_ace(
     language="sql",
-    placeholder="SELECT * FROM `censusdata-team3si.elds_db.acs` limit 10",
+    placeholder="SELECT * FROM ethereum.blocks limit 10",
     theme="twilight",
 )
 
-if ace_query:
-    # Create an empty element to hold the progress bar
-    progress_bar = st.empty()
-
-    # Execute the query and update the progress bar
-    progress_text = "Executing Query..."
-    for percent_complete in range(100):
-        time.sleep(0.05)
-        progress_bar.progress(percent_complete + 1, text=progress_text)
-
-    # Hide the progress bar and display a message
-    progress_bar.empty()
-
-    # Connect to the database and execute the query
-    results_df = query_gcp(ace_query)
-
-    # Display the results
-    st.write(results_df)
+try:
+    if ace_query:
+        results_df = run_query(ace_query, provider)
+        st.write(results_df)
+except:
+    st.write("Write a new query.")
